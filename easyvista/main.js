5692b87c966f3c3c3745892736739f6ac28666e3
Thomas Martinez <tmartinez114@gmail.com>
2021-10-22 20:24:09 -0400

    working on splitting each line in their own arr

--- easyvista/main.js
+++ easyvista/main.js
@@ -1,25 +1,26 @@
+// var fs = require('fs');
+// var movedInfo = [[]];
+
+// function readTextFile() {
+//     fs.readFile('./data/test.txt', 'utf-8', function(err, data) {
+//         if (err) throw err;
+
+//         // console.log(data);
+
+//         // split the text in the file
+//         for (var i = 0; i < 2; i++) {
+//            data.split('\r\n')
+//            console.log(data);
+//         }
+//         // console.log(arrayData);
+
+//     });
+// }
+
+// console.log(readTextFile());
+
 var fs = require('fs');
-var movedInfo = [[]];
+var text = fs.readFileSync('./data/test.txt').toString('utf-8');
+var textByLine = text.split(';');
 
-function readTextFile() {
-    fs.readFile('./data/tickets.txt', 'utf-8', function(err, data) {
-        if (err) throw err;
-
-        // console.log(data);
-
-        // split the text in the file
-        var arrayData = data.split('\n');
-        for (var i = 0; i < arrayData.length; i++) {
-           for (var j = 0; j < arrayData.length; j++) {
-               movedInfo.push(arrayData[i].split(';'));
-           }
-            // console.log('The data on the line', i+1, 'is: ', arrayData[i]);
-        }
-
-        console.log(arrayData);
-        // console.log(`This is subarray 0 from ${arrayData[0]}`);
-
-    });
-}
-
-console.log(readTextFile());
+console.log(textByLine);
